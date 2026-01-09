74694a482081375088b5e81e667fce3377bb8251
NguyenHoangTung <tungc2caugiay@gmail.com>
2026-01-09 13:18:42 +0700

    refactor(jobs): convert sending email function in celery task to synchoronous

--- app/jobs/tasks.py
+++ app/jobs/tasks.py
@@ -1,15 +1,12 @@
-import asyncio
-
+from asgiref.sync import async_to_sync
+from loguru import logger
 from app.jobs.worker import celery
 from app.utils.email import send_verification_email
 
 
 @celery.task
 def send_verification_email_task(email: str, token: str, username: str):
-    print(f"Sending verification email to {email}")
-    loop = asyncio.get_event_loop()
-    if loop.is_closed():
-        loop = asyncio.new_event_loop()
-        asyncio.set_event_loop(loop)
-    loop.run_until_complete(send_verification_email(email, token, username))
-    print(f"Verification email sent to {email}")
+    logger.info(f"Sending verification email to {email}")
+    async_to_sync(send_verification_email)(email, token, username)
+    logger.info(f"Verification email sent to {email}")
+
--- app/jobs/worker.py
+++ app/jobs/worker.py
@@ -1,17 +1,11 @@
-import os
-
 from celery import Celery
-from dotenv import load_dotenv
 
-load_dotenv()
-
-CELERY_BROKER_URL = os.getenv('CELERY_BROKER_URL')
-CELERY_RESULT_BACKEND = os.getenv('CELERY_RESULT_BACKEND')
+from app.core.config import settings
 
 celery = Celery(__name__, include=['app.jobs.tasks'])
 
-celery.conf.broker_url = CELERY_BROKER_URL
-celery.conf.result_backend = CELERY_RESULT_BACKEND
+celery.conf.broker_url = settings.CELERY_BROKER_URL
+celery.conf.result_backend = settings.CELERY_RESULT_BACKEND
 
 celery.conf.update(
     task_serializer='json',
